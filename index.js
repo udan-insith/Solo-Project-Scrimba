011e31ab6d519e4b04592141d70f7a847adf4006
udan-insith <udaninsith23613@gmail.com>
2026-02-18 13:39:19 +0000

    Basic JS to project
    created two JavaScript files and imported a function from fakeStockAPI.js to index.js and some more challenge code

---
+++ index.js
@@ -0,0 +1,14 @@
+import { getStockData } from "./fakeStockAPI.js"
+
+setInterval(function() {
+    const stockData = getStockData()
+    renderStockTicket(stockData)
+}, 1500)
+
+function renderStockTicket(stockData) {
+    const stockDisplayName = document.getElementById('name')
+    const stockDisplaySymbol = document.getElementById('symbol')
+    const stockDisplayPrice = document.getElementById('price')
+    const stockDisplayPriceIcon = document.getElementById('price-icon')
+    const stockDisplayTime = document.getElementById('time')
+}
