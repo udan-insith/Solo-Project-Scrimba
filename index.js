e7e03ff98b68d61279b07416d98c9be2d7db34ad
udan-insith <udaninsith23613@gmail.com>
2026-02-18 14:45:37 +0000

    Main Funcionality Working
    Made a few changes into index files so they are connected now and working properly.

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ function renderStockTicket(stockData) {
     const stockDisplayName = document.getElementById('name')
     const stockDisplaySymbol = document.getElementById('symbol')
     const stockDisplayPrice = document.getElementById('price')
-    const stockDisplayPriceIcon = document.getElementById('price-icon')
+    // const stockDisplayPriceIcon = document.getElementById('price-icon')
     const stockDisplayTime = document.getElementById('time')
 
     const { name, sym, price, time } = stockData
-    
+
+    stockDisplayName.innerText = name
+    stockDisplaySymbol.innerText = sym
+    stockDisplayPrice.innerText = price
+    stockDisplayTime.innerText = time
+
 }
