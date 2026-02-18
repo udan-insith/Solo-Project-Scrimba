4d4d645fe14ea2ba4e7d3b59ba9914807a4c5e4a
udan-insith <udaninsith23613@gmail.com>
2026-02-18 17:15:29 +0000

    Final touches
    added few svg's so we can clearly see if that is a profit or loss or equal. Improved functionality.

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ setInterval(function() {
     renderStockTicket(stockData)
 }, 1500)
 
+let prevPrice = null
+
 function renderStockTicket(stockData) {
     const stockDisplayName = document.getElementById('name')
     const stockDisplaySymbol = document.getElementById('symbol')
     const stockDisplayPrice = document.getElementById('price')
-    // const stockDisplayPriceIcon = document.getElementById('price-icon')
+    const stockDisplayPriceIcon = document.getElementById('price-icon')
     const stockDisplayTime = document.getElementById('time')
 
     const { name, sym, price, time } = stockData
+
+    const priceDirectinIcon = price > prevPrice ? 'green.svg' : price < prevPrice ? 'red.svg' : 'grey.svg'
+    const priceIconElement = document.createElement('img')
+    priceIconElement.src = `svg/${priceDirectinIcon}`
+    priceIconElement.alt = 'Price Direction Icon'
+    stockDisplayPriceIcon.innerHTML = ''
+    stockDisplayPriceIcon.appendChild(priceIconElement)
 
     stockDisplayName.innerText = name
     stockDisplaySymbol.innerText = sym
     stockDisplayPrice.innerText = price
     stockDisplayTime.innerText = time
 
+    prevPrice = price
 }
